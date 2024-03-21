2cfb83df278e305d21dbb9dc98a18cfbc1a20463
Sujal <sujalkesharwani220@gmail.com>
2024-03-21 21:07:37 +0530

    Add icon logo

--- src/Components/Assets/InputBar.jsx
+++ src/Components/Assets/InputBar.jsx
@@ -5,7 +5,7 @@ import GenerateBtn from "./GenerateBtn";
 export default function InputBar({ inputRef, onGenerate }) {
   return (
     <>
-      <div className="search-box flex h-[70px] sm:w-[800px] w-[400px] content-around items-center rounded-[50px] bg-[#0c1846] border-[2px] border-[#181647] ">
+      <div className="search-box flex h-[70px] sm:w-[800px] w-[390px] content-around items-center rounded-[50px] bg-[#0c1846] border-[2px] border-[#181647] ">
         <InputField inputRef={inputRef} />
         <GenerateBtn onGenerate={onGenerate} />
       </div>
--- src/Components/Assets/InputField.jsx
+++ src/Components/Assets/InputField.jsx
@@ -13,7 +13,7 @@ export default function InputField({ inputRef }) {
       <input
         type="text"
         ref={inputRef}
-        className="search-input sm:w-[645px] w-[294px] h-[50px] bg-transparent border-none outline-none text-[18px] text-white sm:pl-[35px] pl-6 sm:pr-[20px] pr-1 placeholder:text-[#999] sm:placeholder:text-xl placeholder:text-lg"
+        className="search-input sm:w-[645px] w-[284px] h-[50px] bg-transparent border-none outline-none text-[18px] text-white sm:pl-[35px] pl-6 sm:pr-[20px] pr-1 placeholder:text-[#999] sm:placeholder:text-xl placeholder:text-lg"
         placeholder="Describe what you want to see..."
       />
     </>
